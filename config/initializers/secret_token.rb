b159f95c4cda13a5e6d99cfb400e135b08734fef
Chris <chris@clear-carbon.co.uk>
2013-09-22 16:06:14 +0100

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+SeatPicker::Application.config.secret_key_base = 'ea923aacfecb9a1a92385bdeefcf246a0115631f4085b08c33eb92e5ce288e6f14e4eef662861ae2f61f7c2731a7f2675b47a3b4facbf84240cd67b4e149e525'
