2c554710b696d311f99244cd590931c54346a0f0
Chris <chris@clear-carbon.co.uk>
2013-10-02 19:44:14 +0100

    adding seeds file

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+rows = {'A' => 4,
+        'B' => 4,
+        'C' => 4,
+        'D' => 4,
+        'E' => 2}
+
+rows.each do |row, seats|
+  for seat in 1..seats
+    Seat.create({:row => row, :number => seat})
+  end
+end
