9864d25a2df0bacedf8623b4b6ffbcb929d6c2f8
Chris <chris@clear-carbon.co.uk>
2013-10-05 10:21:49 +0100

    add pick action to picker

--- app/controllers/picker_controller.rb
+++ app/controllers/picker_controller.rb
@@ -4,4 +4,21 @@ class PickerController < ApplicationController
   def index
     @seats = Seat.order('row asc', 'number asc')
   end
+
+  def pick
+    respond_to do |format|
+      if @seat.update(seat_params)
+        format.json { head :no_content }
+      else
+        format.json { render json: @seat.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  private
+
+  def seat_params
+    params.require(:seat).permit(:user_id)
+  end
+
 end
