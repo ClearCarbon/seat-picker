c4a8b5d75eb26f9d50c459ba89972dcb516c3862
Chris <chris@clear-carbon.co.uk>
2013-10-09 21:42:02 +0100

    remove admin menu for non admin users

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,10 +6,20 @@ class ApplicationController < ActionController::Base
   private
 
   def require_seat_admin
-    unless current_user.email == 'admin@example.com'
+    unless admin?
       flash[:error] = "You do not have permission to do that."
       redirect_to root_path
     end
+  end
+
+  def admin?
+    if signed_in?
+      if current_user.email == 'admin@example.com'
+        return true
+      end
+    end
+
+    return false
   end
 
 end
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def admin?
+    if signed_in?
+      if current_user.email == 'admin@example.com'
+        return true
+      end
+    end
+
+    return false
+  end
 end
