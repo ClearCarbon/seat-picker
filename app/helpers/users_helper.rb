76ba8b1195bc049380fa245002f289a2d6661c60
Chris <chris@clear-carbon.co.uk>
2014-04-27 10:49:23 +0100

    add gravatars to picker

---
+++ app/helpers/users_helper.rb
@@ -0,0 +1,16 @@
+module UsersHelper
+
+  def avatar_for(user, options = {})
+    if !user.nil?
+      avatar_sizes = {micro: 24, normal: 80}
+      default_options = {size: :normal}
+      options = default_options.merge(options)
+      size = avatar_sizes[options[:size]]
+
+      gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
+      gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
+      image_tag(gravatar_url, alt: user.username, class: "gravatar")
+    end
+  end
+
+end
