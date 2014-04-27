eeb2f5383943729515927acbea73324e4f2d0c5d
Chris <chris@clear-carbon.co.uk>
2014-04-27 13:50:49 +0100

    fix seat popover not appearing after the user had taken any action on the picker

--- app/assets/javascripts/picker.js
+++ app/assets/javascripts/picker.js
@@ -6,7 +6,8 @@
       placement: 'top',
       trigger: 'manual'
     });
-    return $('[data-toggle="popover"]').click(function() {
+
+    $('[data-toggle="popover"]').click(function() {
       var clicked;
       clicked = this;
       return $('[data-toggle="popover"]').each(function(index, value) {
@@ -20,42 +21,40 @@
   };
 
   window.pick_seat = function(seat_id) {
-    return $.post('/picker/' + seat_id + '/pick', {}, function(data) {
-      return window.reload_picker();
+    $.post('/picker/' + seat_id + '/pick', {}, function(data) {
+      window.reload_picker();
     });
   };
 
   window.give_up_seat = function() {
-    return $.post('/picker/give_up', {}, function(data) {
-      return window.reload_picker();
+    $.post('/picker/give_up', {}, function(data) {
+      window.reload_picker();
     });
   };
 
   window.request_seat = function(seat_id) {
-    return $.post('/picker/' + seat_id + '/make_request', {}, function(data) {
-      return window.reload_picker();
+    $.post('/picker/' + seat_id + '/make_request', {}, function(data) {
+      window.reload_picker();
     });
   };
 
   window.cancel_request = function(seat_id) {
-    return $.post('/picker/' + seat_id + '/cancel_request', {}, function(data) {
-      return window.reload_picker();
+    $.post('/picker/' + seat_id + '/cancel_request', {}, function(data) {
+      window.reload_picker();
     });
   };
 
   window.donate_seat = function(request_id) {
-    return $.post('/picker/donate_seat', {
+    $.post('/picker/donate_seat', {
       seat_request_id: request_id
     }, function(data) {
-      return window.reload_picker();
+      window.reload_picker();
     });
   };
 
   window.reload_picker = function() {
     $('#seatpicker-management').parent().load('/picker #seatpicker-management');
-    $('#seatpicker-room').load('/picker #seatpicker-room', function() {
-      $(document).ready(ready);
-    });
+    $('#seatpicker-room').load('/picker #seatpicker-room');
   };
 
   $(document).ready(ready);
