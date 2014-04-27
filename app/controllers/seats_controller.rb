4c2eacce9c62e2724cb5685ce0730ef19aa217d8
Chris <chris@clear-carbon.co.uk>
2014-04-27 09:33:59 +0100

    add very simple seat policy

--- app/controllers/seats_controller.rb
+++ app/controllers/seats_controller.rb
@@ -2,27 +2,22 @@ class SeatsController < ApplicationController
   before_filter :authenticate_user!
   before_action :set_seat, only: [:show, :edit, :update, :destroy]
   after_action :verify_authorized, :except => [:index]
+  after_action :verify_policy_scoped, :only => :index
 
-  # GET /seats
-  # GET /seats.json
   def index
-    @seats = Seat.order('row asc', 'number asc')
+    @seats = policy_scope(Seat)
   end
 
-  # GET /seats/new
   def new
     @seat = Seat.new
     authorize @seat, :create?
   end
 
-  # GET /seats/1/edit
   def edit
     @users = User.all
     authorize @seat, :update?
   end
 
-  # POST /seats
-  # POST /seats.json
   def create
     @seat = Seat.new(seat_params)
     authorize @seat, :create?
@@ -38,8 +33,6 @@ class SeatsController < ApplicationController
     end
   end
 
-  # PATCH/PUT /seats/1
-  # PATCH/PUT /seats/1.json
   def update
     authorize @seat, :update?
     respond_to do |format|
@@ -53,8 +46,6 @@ class SeatsController < ApplicationController
     end
   end
 
-  # DELETE /seats/1
-  # DELETE /seats/1.json
   def destroy
     authorize @seat, :destroy?
     @seat.destroy
@@ -65,12 +56,10 @@ class SeatsController < ApplicationController
   end
 
   private
-  # Use callbacks to share common setup or constraints between actions.
   def set_seat
     @seat = Seat.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
   def seat_params
     params.require(:seat).permit(:row, :number, :user_id)
   end
--- app/policies/seat_policy.rb
+++ app/policies/seat_policy.rb
@@ -1,4 +1,11 @@
 class SeatPolicy < Struct.new(:user, :seat)
+
+  class Scope < Struct.new(:current_user, :user)
+    def resolve
+      user.all
+    end
+  end
+
   def update?
     user.admin?
   end
