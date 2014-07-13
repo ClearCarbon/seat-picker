6d5dfbf18595f4507d613b497a3f6e3d0bd3826f
Chris Hunt <chris.hunt@epigenesys.co.uk>
2014-07-13 21:48:07 +0100

    fix js on seat picker not working after pick

--- app/assets/javascripts/picker.js
+++ app/assets/javascripts/picker.js
@@ -1,64 +1,61 @@
-(function() {
-  var ready;
+ready = function() {
+  console.log('testing')
+  $('[data-toggle="popover"]').popover({
+    placement: 'top',
+    trigger: 'manual'
+  });
 
-  ready = function() {
-    $('[data-toggle="popover"]').popover({
-      placement: 'top',
-      trigger: 'manual'
+  $('[data-toggle="popover"]').click(function() {
+    var clicked;
+    clicked = this;
+    $('[data-toggle="popover"]').each(function(index, value) {
+      if (value !== clicked) {
+        $(value).popover('hide');
+      } else {
+        $(clicked).popover('toggle');
+      }
     });
 
-    $('[data-toggle="popover"]').click(function() {
-      var clicked;
-      clicked = this;
-      return $('[data-toggle="popover"]').each(function(index, value) {
-        if (value !== clicked) {
-          return $(value).popover('hide');
-        } else {
-          return $(clicked).popover('toggle');
-        }
-      });
-    });
-  };
+    return false;
+  });
+};
 
-  window.pick_seat = function(seat_id) {
-    $.post('/picker/' + seat_id + '/pick', {}, function(data) {
-      window.reload_picker();
-    });
-  };
+pick_seat = function(seat_id) {
+  $.post('/picker/' + seat_id + '/pick', {}, function(data) {
+    reload_picker();
+  });
+};
 
-  window.give_up_seat = function() {
-    $.post('/picker/give_up', {}, function(data) {
-      window.reload_picker();
-    });
-  };
+give_up_seat = function() {
+  $.post('/picker/give_up', {}, function(data) {
+    reload_picker();
+  });
+};
 
-  window.request_seat = function(seat_id) {
-    $.post('/picker/' + seat_id + '/make_request', {}, function(data) {
-      window.reload_picker();
-    });
-  };
+request_seat = function(seat_id) {
+  $.post('/picker/' + seat_id + '/make_request', {}, function(data) {
+    reload_picker();
+  });
+};
 
-  window.cancel_request = function(seat_id) {
-    $.post('/picker/' + seat_id + '/cancel_request', {}, function(data) {
-      window.reload_picker();
-    });
-  };
+cancel_request = function(seat_id) {
+  $.post('/picker/' + seat_id + '/cancel_request', {}, function(data) {
+    reload_picker();
+  });
+};
 
-  window.donate_seat = function(request_id) {
-    $.post('/picker/donate_seat', {
-      seat_request_id: request_id
-    }, function(data) {
-      window.reload_picker();
-    });
-  };
+donate_seat = function(request_id) {
+  $.post('/picker/donate_seat', {
+    seat_request_id: request_id
+  }, function(data) {
+    reload_picker();
+  });
+};
 
-  window.reload_picker = function() {
-    $('#seatpicker-management').parent().load('/picker #seatpicker-management');
-    $('#seatpicker-room').load('/picker #seatpicker-room');
-  };
+reload_picker = function() {
+  $('#seatpicker-management').parent().load('/picker #seatpicker-management');
+  $('#seatpicker-room').load('/picker #seatpicker-room');
+};
 
-  $(document).ready(ready);
-
-  $(document).on('page:load', ready);
-
-}).call(this);
+$(document).ready(ready);
+$(document).on('page:load', ready);
