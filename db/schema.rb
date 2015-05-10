bda9d2d9d1ff8371b3b72be519940758b2fb3310
Chris Hunt <chris.hunt@epigenesys.co.uk>
2015-05-10 15:54:20 +0100

    remove picker background

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20140901112051) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "seat_requests", force: true do |t|
     t.integer  "seat_id"
     t.integer  "user_id"
