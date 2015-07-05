fdc12b7054945f6f8d6513dad3491e8f6523df59
Chris Hunt <chris@clear-carbon.co.uk>
2015-07-05 15:31:22 +0100

    update helper to prepare test schema

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -27,6 +27,7 @@ require 'capybara/poltergeist'
 
 # Checks for pending migrations before tests are run.
 # If you are not using ActiveRecord, you can remove this line.
+ActiveRecord::Migration.maintain_test_schema!
 ActiveRecord::Migration.check_pending!
 
 RSpec.configure do |config|
