387dd20f0582e91047aa6ad90d703da3ed8a5b41
Chris Hunt <chris@clear-carbon.co.uk>
2016-01-04 22:46:41 +0000

    fix restricted registration not working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,6 +6,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_action :configure_permitted_parameters, if: :devise_controller?
   rescue_from Pundit::NotAuthorizedError, with: :user_not_authorized
+  helper_method :registration_allowed?
 
   protected
 
@@ -19,13 +20,17 @@ class ApplicationController < ActionController::Base
 
   private
 
-  def configure_permitted_parameters
-    devise_parameter_sanitizer.for(:sign_up) << :username
-    devise_parameter_sanitizer.for(:account_update) << :username
-  end
+    def configure_permitted_parameters
+      devise_parameter_sanitizer.for(:sign_up) << :username
+      devise_parameter_sanitizer.for(:account_update) << :username
+    end
 
-  def user_not_authorized
-    flash[:error] = 'You are not authorized to perform this action.'
-    redirect_to(request.referrer || root_path)
-  end
+    def user_not_authorized
+      flash[:error] = 'You are not authorized to perform this action.'
+      redirect_to(request.referrer || root_path)
+    end
+
+    def registration_allowed?
+      ENV['restricted_registration'] == 'false' || ENV['restricted_registration_key'] == params[:key]
+    end
 end
--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,15 +1,15 @@
 class RegistrationsController < Devise::RegistrationsController
-  before_filter :registration_allowed!, except: [:edit, :update]
+  before_filter :ensure_registration_allowed!, except: [:edit, :update]
+  helper_method :registration_allowed?
 
-  def ensure_registration_key!
-    key = ENV['restricted_registration_key']
-    given_key = params[:key]
-
-    redirect_to :root if key != given_key
+  def ensure_registration_allowed!
+    redirect_to :root unless registration_allowed?
   end
 
-  def registration_allowed!
-    restricted = ENV['restricted_registration']
-    return ensure_registration_key! if restricted == true
-  end
+  private
+  
+    def registration_allowed?
+      ENV['restricted_registration'] == 'false' || ENV['restricted_registration_key'] == params[:key]
+    end
+    
 end
